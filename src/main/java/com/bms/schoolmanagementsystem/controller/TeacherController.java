2d68b360f6b87649fc4466a4eac4bc4342ef4b21
alexdongmo <dongmoalex59@gmail.com>
2024-08-15 08:35:04 +0100

    refactoring source code

--- src/main/java/com/bms/schoolmanagementsystem/controller/TeacherController.java
+++ src/main/java/com/bms/schoolmanagementsystem/controller/TeacherController.java
@@ -1,11 +1,15 @@
 package com.bms.schoolmanagementsystem.controller;
 
 import com.bms.schoolmanagementsystem.dto.TeacherDto;
+import com.bms.schoolmanagementsystem.dto.converter.TeacherDtoConverter;
 import com.bms.schoolmanagementsystem.dto.request.teacher.CreateTeacherRequest;
 import com.bms.schoolmanagementsystem.dto.request.teacher.UpdateTeacherRequest;
+import com.bms.schoolmanagementsystem.model.Teacher;
 import com.bms.schoolmanagementsystem.service.TeacherService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,52 +21,44 @@ import java.util.List;
 @Tag(name = "Teacher", description = "Teacher API")
 public class TeacherController {
     private final TeacherService teacherService;
+    private final TeacherDtoConverter converter;
 
-    public TeacherController(TeacherService teacherService) {
+    public TeacherController(TeacherService teacherService, TeacherDtoConverter converter) {
         this.teacherService = teacherService;
+        this.converter = converter;
     }
 
-    @Operation(summary = "Create Teacher",
-            description = "Create Teacher",
-            tags = {"Teacher"})
+    @Operation(summary = "Create Teacher", description = "Create Teacher", tags = { "Teacher" })
     @PostMapping
-    public ResponseEntity<Void> createTeacher(@Valid CreateTeacherRequest request) {
-        teacherService.createTeacher(request);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<TeacherDto> createTeacher(@Valid CreateTeacherRequest request) {
+        Teacher teacher = teacherService.createTeacher(request);
+        return new ResponseEntity<>(converter.convert(teacher), HttpStatus.CREATED);
     }
 
-    @Operation(summary = "Update Teacher",
-            description = "Update Teacher by id",
-            tags = {"Teacher"})
+    @Operation(summary = "Update Teacher", description = "Update Teacher by id", tags = { "Teacher" })
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateTeacher(@PathVariable String id,
-                                              @Valid UpdateTeacherRequest request) {
-        teacherService.updateTeacher(id, request);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<TeacherDto> updateTeacher(@PathVariable String id,
+            @Valid UpdateTeacherRequest request) {
+        Teacher teacher = teacherService.updateTeacher(id, request);
+        return ResponseEntity.ok(converter.convert(teacher));
     }
 
-    @Operation(summary = "Delete Teacher",
-            description = "Delete Teacher by id",
-            tags = {"Teacher"})
+    @Operation(summary = "Delete Teacher", description = "Delete Teacher by id", tags = { "Teacher" })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTeacher(@PathVariable String id) {
         teacherService.deleteTeacher(id);
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "Get Teacher",
-            description = "Get Teacher by id",
-            tags = {"Teacher"})
+    @Operation(summary = "Get Teacher", description = "Get Teacher by id", tags = { "Teacher" })
     @GetMapping("/{id}")
     public ResponseEntity<TeacherDto> findTeacherById(@PathVariable String id) {
-        return ResponseEntity.ok(teacherService.findTeacherById(id));
+        return ResponseEntity.ok(converter.convert(teacherService.findTeacherById(id)));
     }
 
-    @Operation(summary = "Get All Teachers",
-            description = "Get All Teachers",
-            tags = {"Teacher"})
+    @Operation(summary = "Get All Teachers", description = "Get All Teachers", tags = { "Teacher" })
     @GetMapping
     public ResponseEntity<List<TeacherDto>> findAllTeachers() {
-        return ResponseEntity.ok(teacherService.findAllTeachers());
+        return ResponseEntity.ok(converter.convert(teacherService.findAllTeachers()));
     }
 }
