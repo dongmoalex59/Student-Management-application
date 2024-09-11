4c00b2e6e8d14706e236e28badaa10a0e9d211eb
Daniel <nongadaniel41@gmail.com>
2024-09-11 15:55:59 +0200

    mon travail

---
+++ src/main/java/com/bms/schoolmanagementsystem/model/Subject.java
@@ -0,0 +1,25 @@
+package com.bms.schoolmanagementsystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Subject {
+     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+    private String intitule;
+    private int coefficient;
+
+}
